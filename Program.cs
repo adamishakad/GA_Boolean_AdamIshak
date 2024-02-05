748fa276f57fd2057aec3b0b1d37c51dc146a98a
adamishakad <155333634+adamishakad@users.noreply.github.com>
2024-02-04 20:33:59 -0800

    Updated Program File

--- Program.cs
+++ Program.cs
@@ -1,10 +1,41 @@
-﻿namespace GA_Boolean_AdamIshak
+﻿// Adam Ishak
+namespace GA_Boolean_AdamIshak
 {
     internal class Program
     {
+        // Entry point of our C# program
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("true");
+
+            bool isAdult = true;
+
+            Console.WriteLine(isAdult);
+
+            if (isAdult)
+             Console.WriteLine("You are an adult.");
+
+            isAdult = false;
+            Console.WriteLine(isAdult);
+
+            bool hasLicense = true;
+            if (isAdult && hasLicense)
+                Console.WriteLine("You are an adult and have a license.");
+            
+            else 
+            Console.WriteLine("Condition not met.");
+
+
         }
+       
+       
+        
+        
+
+            
+        
+
+        
+
     }
 }
